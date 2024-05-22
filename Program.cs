1c228ae247955cd204cd64d7f8a505aacd976b37
JongD <JongD@JONGDO>
2024-05-22 07:09:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace assignment3._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+            int[,] numbers2 = { { 1, 1, 5, 4 }, { 7, 6, 7, 8 } };
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                Console.Write("| ");
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.Write(numbers[i, j] + numbers2[i, j] + " | ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
